85e3c725f34e7de29f45ba1acace998002e6287b
Navdeep Sharma <gladiator.letsdie@gmail.com>
2016-03-07 06:12:36 -0800

    assignment 3

--- webmining/prefAttach.py
+++ webmining/prefAttach.py
@@ -1,28 +1,23 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jan 25 19:40:10 2016
+Created on Mon Feb 15 14:15:49 2016
 
-@author: hp
+@author: navdeep
 """
-import csv
-import re
+
+#import csv
+#import re
 import matplotlib.pyplot as plt
 import networkx as nx
-import numpy as nm
-from operator import itemgetter
+#from operator import itemgetter
 from networkx.algorithms import bipartite
-from networkx.utils import (powerlaw_sequence, create_degree_sequence)
+#from networkx.utils import (powerlaw_sequence, create_degree_sequence)
+import numpy as nm
+#import plotly.plotly as py
 
 
-G=nx.Graph()
 
-
-with open('/home/navdeep/python/data/1out.contiguous-usa', 'r') as f:
-    reader = csv.reader(f)
-    for row in reader:
-            nodes=re.split(r' +', row[0])
-            G.add_edge(nodes[0],nodes[1])
-
+G=nx.barabasi_albert_graph(49,4)
 
 #print(G1.edges())
 #print(nx.diameter(G1, e=None))# graph not connected
@@ -30,19 +25,16 @@ with open('/home/navdeep/python/data/1out.contiguous-usa', 'r') as f:
 print(bipartite.is_bipartite(G))
 d = nx.degree(G)
 #nx.draw(G, nodelist=d.keys())
-print(nx.diameter(G, e=None))
-print("clustering coefficient",nx.average_clustering(G))
-print("AVG",nx.average_shortest_path_length(G))
+#print(nx.diameter(G, e=None))
 nx.draw(G, nodelist=d.keys(), node_size=[v * 20 for v in d.values()])
-plt.savefig("./USA/figure1.png")
+plt.savefig("plotprefAttach.png")
 plt.show()
-'''print('diameter ',nx.diameter(G, e=None))# graph not connected
+print('diameter ',nx.diameter(G, e=None))# graph not connected
 print('debsity', nx.density(G))
 print("clustering coefficient",nx.average_clustering(G))
 #print("average degree ", nm.mean(G.degree()))
 print("AVG",nx.average_shortest_path_length(G))
 print(" Clique ",len(list(nx.find_cliques(G))))
-
 
 L = nx.normalized_laplacian_matrix(G)
 e = nm.linalg.eigvals(L.A)
@@ -50,33 +42,29 @@ e = nm.linalg.eigvals(L.A)
 print("Largest eigenvalue:", max(e))
 print("Smallest eigenvalue:", min(e))
 
-centrality = nx.eigenvector_centrality(G,1000)
+centrality = nx.eigenvector_centrality(G)
 print(['%s %0.2f'%(node,centrality[node]) for node in centrality])
 #plt.plot(node,centrality[node])
 
 plt.bar(range(len(centrality)), centrality.values(), align='center')
 plt.xticks(range(len(centrality)), centrality.keys())
 plt.show()
-plt.savefig("./assignment3/eigenvectorcentralityUSA.png")
-
-
-
-
 pager=nx.pagerank(G)
 print('Page rank',pager )
 
 plt.bar(range(len(pager)), pager.values(), align='center')
 plt.xticks(range(len(pager)), pager.keys())
 plt.show()
-'''
+
 kz=nx.katz_centrality_numpy(G,0.62)
 print('Katz centrality', kz)
 plt.bar(range(len(kz)), kz.values(), align='center')
 plt.xticks(range(len(kz)), kz.keys())
 plt.show()
-#print(lcc)
 
-print(nx.degree_centrality(G))
+#plt.savefig("eigenvectorcentralityPA.png")
+
+#print(nx.degree_centrality(G))
 #code for degree distribution
 
 degree_sequence=sorted(nx.degree(G).values(),reverse=True)
@@ -89,10 +77,11 @@ plt.axes([0.45,0.45,0.45,0.45])
 Gcc=sorted(nx.connected_component_subgraphs(G), key = len, reverse=True)[0]
 pos=nx.spring_layout(Gcc)
 plt.axis('off')
+print("Diameter", nx.diameter(Gcc, e=None))
 nx.draw_networkx_nodes(Gcc,pos,node_size=20)
 nx.draw_networkx_edges(Gcc,pos,alpha=0.4)
 
-plt.savefig("./USA/degree_histogram_usa.png")
+#plt.savefig("degreeprefAttach.png")
 plt.show()
-print(nx.average_neighbor_degree(G, source='in', target='in'))
+#print(nx.average_neighbor_degree(G, source='in', target='in'))
 
--- webmining/smallworld.py
+++ webmining/smallworld.py
@@ -1,32 +1,34 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jan 25 19:40:10 2016
+Created on Mon Feb 15 16:46:21 2016
 
-@author: hp
+@author: navdeep
 """
-import csv
-import re
+
+
+#import csv
+#import re
 import matplotlib.pyplot as plt
 import networkx as nx
 from operator import itemgetter
 from networkx.algorithms import bipartite
 from networkx.utils import (powerlaw_sequence, create_degree_sequence)
-#from igraph import Graph, mean
+import numpy.linalg
 import numpy as nm
 
-#G=nx.gnm_random_graph(2939,30501,directed=True)
-D = nx.gnr_graph(49, 0.09083)  # the GNR graph
-G = D.to_undirected()  # the undirected version
 
+G=nx.watts_strogatz_graph(49,4,0.05)
 
+#print(G1.edges())
+#print(nx.diameter(G1, e=None))# graph not connected
 #nx.draw_random(G1)
 print(bipartite.is_bipartite(G))
 d = nx.degree(G)
-nx.draw(G, nodelist=d.keys())
-#nx.draw(G, nodelist=d.keys(), node_size=[v * 20 for v in d.values()])
-#plt.savefig("./random/sameNodesize.png")
+#nx.draw(G, nodelist=d.keys())
+#print(nx.diameter(G, e=None))
+nx.draw(G, nodelist=d.keys(), node_size=[v * 20 for v in d.values()])
+plt.savefig("smallworld.png")
 plt.show()
-
 print('diameter ',nx.diameter(G, e=None))# graph not connected
 print('debsity', nx.density(G))
 print("clustering coefficient",nx.average_clustering(G))
@@ -34,12 +36,21 @@ print("clustering coefficient",nx.average_clustering(G))
 print("AVG",nx.average_shortest_path_length(G))
 print(" Clique ",len(list(nx.find_cliques(G))))
 
+
 L = nx.normalized_laplacian_matrix(G)
 e = nm.linalg.eigvals(L.A)
 
 print("Largest eigenvalue:", max(e))
 print("Smallest eigenvalue:", min(e))
 
+centrality = nx.eigenvector_centrality(G,1000)
+print(['%s %0.2f'%(node,centrality[node]) for node in centrality])
+#plt.plot(node,centrality[node])
+
+plt.bar(range(len(centrality)), centrality.values(), align='center')
+plt.xticks(range(len(centrality)), centrality.keys())
+plt.show()
+#plt.savefig("eigenvectorcentralitySW.png")
 
 
 pager=nx.pagerank(G)
@@ -49,35 +60,22 @@ plt.bar(range(len(pager)), pager.values(), align='center')
 plt.xticks(range(len(pager)), pager.keys())
 plt.show()
 
-centrality = nx.eigenvector_centrality(G,100000)
-print(['%s %0.2f'%(node,centrality[node]) for node in centrality])
-#plt.plot(node,centrality[node])
-
-plt.bar(range(len(centrality)), centrality.values(), align='center')
-plt.xticks(range(len(centrality)), centrality.keys())
-plt.show()
-
-#plt.savefig("./assignment3/eigenvectorcentralityRG.png")
-kz=nx.katz_centrality_numpy(G,0.62)
+kz=nx.katz_centrality_numpy(G,0.60)
 print('Katz centrality', kz)
 plt.bar(range(len(kz)), kz.values(), align='center')
 plt.xticks(range(len(kz)), kz.keys())
 plt.show()
+#print(nx.degree_histogram(G))
+'''egV=nx.eigenvector_centrality(G,5000)
+for key, value in egV.iteritems() :
+    print key, value
 
-loops = G.selfloop_edges()
-# remove parallel edges and self-loops
-graph = nx.Graph(G)
-graph.remove_edges_from(loops)
-# get largest connected component
-# unfortunately, the iterator over the components is not guaranteed to be sorted by size
-components = sorted(nx.connected_components(graph), key=len, reverse=True)
-lcc = graph.subgraph(components[0])
-pos=nx.spring_layout(lcc)
-d = nx.degree(lcc)
-#nx.draw(lcc, nodelist=d.keys(), node_size=[v * 20 for v in d.values()])
-#nx.draw_networkx_labels(lcc,pos=nx.spring_layout(lcc))
-#plt.show()
-# code for histogram
+e = numpy.linalg.eigvals(egV)
+print(e)
+centrality = nx.katz_centrality(G,1/max(e)-0.01)
+'''
+#print(nx.degree_centrality(G))
+#code for degree distribution
 
 degree_sequence=sorted(nx.degree(G).values(),reverse=True)
 dmax=max(degree_sequence)
@@ -89,10 +87,11 @@ plt.axes([0.45,0.45,0.45,0.45])
 Gcc=sorted(nx.connected_component_subgraphs(G), key = len, reverse=True)[0]
 pos=nx.spring_layout(Gcc)
 plt.axis('off')
+print("Diameter", nx.diameter(Gcc, e=None))
 nx.draw_networkx_nodes(Gcc,pos,node_size=20)
 nx.draw_networkx_edges(Gcc,pos,alpha=0.4)
 
-plt.savefig("./USA/degree_histogram_usa.png")
-plt.show()
-print(nx.average_neighbor_degree(G, source='in', target='in'))
+#plt.savefig("smallworld-degree.png")
+#plt.show()
+#print(nx.average_neighbor_degree(G, source='in', target='in'))
 
